fab327d0524c5ad401ceca24e4a0a369d7d3d9d6
numberwang55 <cban0901@hotmail.com>
2023-08-06 15:34:46 +0100

    sound picker buttons working

--- app.js
+++ app.js
@@ -1,55 +1,70 @@
 const app = () => {
-    const song = document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video');
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
 
-    const sounds = document.querySelectorAll('.sound-picker button')
-    const timeDisplay = document.querySelector('.time-display')
-    const timeSelect = document.querySelectorAll('.time-select button')
-    const outlineLength = outline.getTotalLength()
-    let fakeDuration = 600;
-    console.log(typeof(fakeDuration));
+  const sounds = document.querySelectorAll(".sound-picker button");
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
+  const outlineLength = outline.getTotalLength();
+  let fakeDuration = 600;
+  console.log(typeof fakeDuration);
 
-    outline.style.strokeDasharray = outlineLength;
-    outline.style.strokeDashoffset = outlineLength;
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
 
-    play.addEventListener('click', () => {
+  sounds.forEach((sound) => {
+    sound.addEventListener("click", function () {
+        song.src = this.getAttribute("data-sound");
+        video.src = this.getAttribute("data-video");
         checkPlaying(song)
     })
+  })
 
-    timeSelect.forEach(option => {
-        option.addEventListener('click', function() {
-            fakeDuration = this.getAttribute('data-time');
-            console.log(fakeDuration);
-            console.log(`${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`);
-            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}0`
-        })
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  timeSelect.forEach((option) => {
+    option.addEventListener("click", function () {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}0`;
     });
+  });
 
-    const checkPlaying = (song) => {
-        if (song.paused) {
-            song.play()
-            video.play()
-            play.src = './svg/pause.svg'
-        } else {
-            song.pause()
-            video.pause()
-            play.src = './svg/play.svg'
-        }
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
     }
+  };
 
-    song.ontimeupdate = () => {
-        let currentTime = song.currentTime;
-        let elapsed = fakeDuration - currentTime;
-        let seconds = Math.floor(elapsed % 60);
-        let minutes = Math.floor(elapsed / 60);
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
 
-        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-        outline.style.strokeDashoffset = progress;
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
 
-        timeDisplay.textContent = `${minutes}:${seconds}`
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause()
     }
-}
+  };
+};
 
 app();
